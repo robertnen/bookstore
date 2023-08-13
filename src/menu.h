5626fe26976f12797b22a0708d441363fc8244c2
Lucky8boy <robert.nenciu@yahoo.com>
2023-08-13 15:27:33 +0300

    Fixed menu.h option 5
    Yes, I forgot that it is a store and not a library lol

--- src/menu.h
+++ src/menu.h
@@ -12,10 +12,10 @@
             const std::string sWelcome = "Welcome to my bookstore!\n";
             const int option = 5; //add, remove, check, buy, loan
             const std::string sOptions[5] = {"1. Add book\n",
-                                            "2. Remove book\n",
-                                            "3. Check if book exists\n",
-                                            "4. Buy book\n",
-                                            "5. Loan book\n"};
+                                             "2. Remove book\n",
+                                             "3. Check if book exists\n",
+                                             "4. Buy book\n",
+                                             "5. Exit\n"};
             short choice;
         public:
             void welcome();
