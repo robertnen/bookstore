2ee81d8e7be6427c192d849bdedef5d40267e045
Lucky8boy <robert.nenciu@yahoo.com>
2023-08-14 01:08:38 +0300

    Testing in main the functions from Book class
    'createFile' and 'destroyFile' work fine now

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ int main() {
 
     Book book1(2000, 2, "Name", "Author", "Lorem ipsum"), book2(1900, 1, "Name2", "Author2"), book3(1945, 15, "Name3");
     // book1.displayBook();
+    book1.createFile();
+    book1.destroyFile();
     // book2.displayBook();
     // book3.displayBook();
 
