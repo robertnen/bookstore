cb94686c55767ff9611c4da9e90283a34f8587fb
Lucky8boy <robert.nenciu@yahoo.com>
2023-08-14 01:05:17 +0300

    Added prototype for 'delay seconds' + some comment changes

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,15 +1,13 @@
     #include "menu.h"
 
-    // checks if a string begins either with 1, 2, 3, 4 or 5.
     bool isValid(std::string str) {
         std::regex patttern("^[12345]");
         return std::regex_search(str, patttern);
     }
 
-    // it makes the program to sleep for a number of seconds
     void delaySeconds(int seconds) {
         unsigned int timeToGet = time(0) + seconds;
-        while(time(0) < timeToGet);   //waiting for the seconds to pass
+        while(time(0) < timeToGet);   // waiting for the seconds to pass
     }
 
     void Menu::welcome() {
--- src/menu.h
+++ src/menu.h
@@ -5,21 +5,26 @@
     #include <cstring>
     #include <regex>
 
+    // checks while using regex if a string contains at start 1, 2, 3, 4 or 5
     bool isValid(std::string str);
+
+    // it makes the program to sleep for a number of seconds
+    void delaySeconds(int seconds);
 
     class Menu {
         private:
             const std::string sWelcome = "Welcome to my bookstore!\n";
-            const int option = 5; //add, remove, check, buy, loan
+            const int option = 5;    //add, remove, check, buy, loan
             const std::string sOptions[5] = {"1. Add book\n",
                                              "2. Remove book\n",
                                              "3. Check if book exists\n",
                                              "4. Buy book\n",
                                              "5. Exit\n"};
             short choice;
+
         public:
-            void welcome();
-            void getOptions();
+            void welcome();         // welcome message
+            void getOptions();      // get options menu
     };
 
 #endif
