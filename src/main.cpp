58d5389c0f25f4c5fe1cc941f6ebdc46be0aa265
Lucky8boy <robert.nenciu@yahoo.com>
2023-08-14 22:09:19 +0300

    Updated Main function
    - no more tests on it
    - 'bookstore is finally open'
    - have fun

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,34 @@
 int main() {
 
     Menu menu;
-    // menu.welcome();
-    // menu.getOptions();
 
-    menu.addBookOption();
+    menu.welcome();
+    while(menu.getChoice() != 5) {
+        menu.getOptions();
+
+        switch(menu.getChoice()) {
+            case 1:
+                menu.addBookOption();
+                break;
+            case 2:
+                menu.removeBookOption();
+                break;
+            case 3:
+                menu.searchBookOption();
+                break;
+            case 4:
+                menu.buyBookOption();
+                break;
+            case 5:
+                std::cout << "Thanks for looking in our bookstore! Have a nice day!\n";
+                break;
+            case -1:
+                break; // do nothing
+            default:
+                std::cout << "Something bad happen. Please send an issue about it!\n";
+                break;
+        }
+    }
+
     return 0;
 }
